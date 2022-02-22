88bb36d263201dea6f6672e233b363f9e00b6a23
meow854 <81199224+meow854@users.noreply.github.com>
2022-02-22 12:09:19 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+function preload() {
+
+}
+
+function setup() {
+    qwe= createCanvas(1478, 597);
+}
+
+function draw() {
+    circle(350, 100, 60);
+    circle(350, 100, 40);
+    circle(350, 100, 20);
+    fill(148, 86, 132);
+    stroke(255,255,255);
+    rect(375, 89, 740, 20);
+    rect(1140, 89, 20, 400);
+    rect(375, 500, 740, 20);
+    rect(340, 125, 20, 400);
+    fill(255, 196, 226);
+    circle(740, 100, 60);
+    circle(740, 100, 40);
+    circle(740, 100, 20);
+    circle(1140, 100, 60);
+    circle(1140, 100, 40);
+    circle(1140, 100, 20);
+    circle(1140, 500, 60);
+    circle(1140, 500, 40);
+    circle(1140, 500, 20);
+    circle(740, 500, 60);
+    circle(740, 500, 40);
+    circle(740, 500, 20);
+    circle(350, 500, 60);
+    circle(350, 500, 40);
+    circle(350, 500, 20);
+}
